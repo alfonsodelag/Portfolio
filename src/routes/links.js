e00a21c06e2666de31b96351929478727c9a50c3
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-01-30 15:18:05 +0100

    edit entries 1:53

--- src/routes/links.js
+++ src/routes/links.js
@@ -34,5 +34,25 @@ router.get('/delete/:id', async (req,res) => {
     res.redirect('/links');
 });
 
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    console.log(links[0]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = {
+        title, 
+        description,
+        url
+    };
+    console.log(newLink);
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    res.redirect('/links');
+});
+
 module.exports = router;
 
