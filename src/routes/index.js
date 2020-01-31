44596a9fcc7ee53440c4433612b85d6ceb78d121
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-01-31 12:47:22 +0100

    styling added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 });
 module.exports = router;
